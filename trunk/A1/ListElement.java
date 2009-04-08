13b4dc69b0f7364d6e3f1f06f2eae4502cf32020
geoff.oxholm <geoff.oxholm@c6e0ff0a-2120-11de-a108-cd2f117ce590>
2009-04-08 13:36:23 +0000

    minor changes, some fixes to makefile

--- trunk/A1/ListElement.java
+++ trunk/A1/ListElement.java
@@ -1,8 +1,15 @@
-/*
+/**
  *  Program:  ListElement.java
  *  Authors:  Jordan Osecki, Joseph Conley, Geoff Oxholm, and Rich Price
  *  Class:    CS550, Assignment 1, Spring 2009
  *  Description:  ListElement Class which can be built from parser and can print
- */
+ **/
 
-public class ListElement{};
+public abstract class ListElement{
+    public void print() {
+        System.out.println(toString());
+    }
+
+    /// Every ListElement sub-class must have this method
+    public abstract String toString();
+}
--- trunk/A1/NumberListElement.java
+++ trunk/A1/NumberListElement.java
@@ -1,9 +1,9 @@
-/*
+/**
  *  Program:  NumberListElement.java
  *  Authors:  Jordan Osecki, Joseph Conley, Geoff Oxholm, and Rich Price
  *  Class:    CS550, Assignment 1, Spring 2009
  *  Description:  NumberListElement Class which can be built from parser and can print
- */
+ **/
 
 public class NumberListElement extends ListElement {
     protected Number n;
