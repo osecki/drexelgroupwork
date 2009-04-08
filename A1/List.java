9f78cb52d6e233938cfdd779ac82dcfbb616aa70
geoff.oxholm <geoff.oxholm@c6e0ff0a-2120-11de-a108-cd2f117ce590>
2009-04-08 13:36:23 +0000

    minor changes, some fixes to makefile

--- A1/List.java
+++ A1/List.java
@@ -1,9 +1,9 @@
-/*
+/**
  *  Program:  List.java
  *  Authors:  Jordan Osecki, Joseph Conley, Geoff Oxholm, and Rich Price
  *  Class:    CS550, Assignment 1, Spring 2009
  *  Description:  List Class which can be built from parser and can print
- */
+ **/
 
 public class List extends ListElement {
     private Sequence elements;
--- A1/Sequence.java
+++ A1/Sequence.java
@@ -1,9 +1,9 @@
-/*
+/**
  *  Program:  Sequence.java
  *  Authors:  Jordan Osecki, Joseph Conley, Geoff Oxholm, and Rich Price
  *  Class:    CS550, Assignment 1, Spring 2009
  *  Description:  Sequence Class which can be built from parser and can print
- */
+ **/
 
 import java.util.Vector;
 
