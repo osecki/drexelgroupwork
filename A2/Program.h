bde99f536a1feb2f9021335d22555a3a0fc006a4
jordan.osecki <jordan.osecki@c6e0ff0a-2120-11de-a108-cd2f117ce590>
2009-04-18 18:26:56 +0000

    Moved the definitions into each file to be consistent with List. I think it's easy to follow this way. Plan to continue doing more...

--- A2/Program.h
+++ A2/Program.h
@@ -11,10 +11,33 @@ using namespace std;
 class Program
 {
 public:
-    Program(StmtList *SL);
-    ~Program() {delete SL_; };
-    void dump();
-    void eval();
+    Program(StmtList *SL)
+    {
+        NameTable_.clear();
+        FunctionTable_.clear();
+        SL_ = SL;
+    }
+
+    ~Program() {delete SL_; }
+
+    void dump()
+    {
+        map<string,int>::iterator p;
+        map<string,Proc*>::iterator f;
+
+        cout << "Dump of Symbol Table" << endl;
+        cout << "Name Table" << endl;
+        for (p = NameTable_.begin();p != NameTable_.end();p++)
+            cout << p->first << " -> " << p->second << endl;
+        cout << "Function Table" << endl;
+        for (f = FunctionTable_.begin();f != FunctionTable_.end();f++)
+        cout << f->first << endl;
+    }
+
+    void eval()
+    {
+	  SL_->eval(NameTable_, FunctionTable_);
+    }
 
 private:
     StmtList *SL_;
