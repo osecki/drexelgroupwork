a9e1626f8e0a756d01304b37ad56d53c4b71cdfd
jordan.osecki <jordan.osecki@c6e0ff0a-2120-11de-a108-cd2f117ce590>
2009-04-22 20:48:06 +0000

    Fixed last bug, A2 DONE

--- trunk/A2/IfStmt.cpp
+++ trunk/A2/IfStmt.cpp
@@ -1,6 +1,8 @@
 #include "IfStmt.h"
 #include "StmtList.h"
 #include "Proc.h"
+#include "Expr.h"
+#include "Number.h"
 #include <map>
 
 IfStmt::IfStmt(Expr *E, StmtList *S1, StmtList *S2)
@@ -15,7 +17,7 @@ IfStmt::~IfStmt() { delete E_; delete S1_; delete S2_; }
 // Changed environment table here
 void IfStmt::eval(map<string,Element*> &NT, map<string,Proc*> &FT) const
 {
-    if (E_->eval(NT,FT) > 0)
+    if (((Number*)E_->eval(NT,FT))->getValue() > 0)
         S1_->eval(NT,FT);
     else
         S2_->eval(NT,FT);
