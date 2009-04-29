8c6811e5b6aa6fc25d72ba463979f3237d5987fe
jordan.osecki <jordan.osecki@c6e0ff0a-2120-11de-a108-cd2f117ce590>
2009-04-29 21:38:47 +0000

    Has debug print name table too, updated sample_input

--- A3/StmtList.cpp
+++ A3/StmtList.cpp
@@ -1,8 +1,9 @@
 #include "StmtList.h"
 #include "Stmt.h"
 #include "Program.h"
+#include "Element.h"
 #include <map>
-
+#include <iomanip>
 
 void StmtList::insert(Stmt * S)
 {
@@ -17,8 +18,21 @@ void StmtList::eval(map<string,Element*> &NT, map<string,Proc*> &FT, Memory &mem
     for (Sp = SL_.begin();Sp != SL_.end();Sp++) {
         (*Sp)->eval(NT,FT,memory);
         if(DEBUG) {
+					  // Print out Memory table after current statement
             cout << "Memory table after statement " << statement++ << endl;
             memory.output();
+						
+						// Print out Name table after current statement
+						map<string,Element*>::iterator p;
+						cout << "Name Table" << endl;
+						cout << right << setw(10) << "Name" << setw(14) << "Value" << setw(14) << "List Value" << endl;
+					  for (p = NT.begin();p != NT.end();p++) {
+								cout << setw(10) << p->first;
+								if(p->second->listp()) {
+										cout << setw(13) <<  p->second->getAddress() << setw(1) << "*";
+								}
+										cout << setw(14) << p->second->toString(memory) << endl;
+						}
         }
     }
 }
