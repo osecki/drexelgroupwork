b1039d008167f23853b36d43297138140c81be33
geoff.oxholm <geoff.oxholm@c6e0ff0a-2120-11de-a108-cd2f117ce590>
2009-04-30 16:37:31 +0000

    Revision 100 boo yah\! eat it\!

--- trunk/A3/AssignStmt.cpp
+++ trunk/A3/AssignStmt.cpp
@@ -11,14 +11,14 @@ AssignStmt::AssignStmt(string name, Expr *E)
 // Changed environment table here
 void AssignStmt::eval(map<string,Element*> &NT, map<string,Proc*> &FT, Memory &memory) const
 {
+    #if 0 // This was removed to allow cyclical definitons (like count := count + 1) - Geoff
     // Incase we are over-writing a value, remove the value from the name table
     // so that its memory (if any) will not be protected during garbage collection
-    #if 0
     map<string,Element*>::iterator location = NT.find(name_);
     if(location != NT.end()) {
         NT.erase(location);
     }
-    #endif // This was removed to allow cyclical definitons (like count := count + 1) - Geoff
+    #endif
 
     // Update the name table with the new Element
     NT[name_] = E_->eval(NT,FT,memory);
