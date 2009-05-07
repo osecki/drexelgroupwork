363026d6fa265bed3a0c5e025c9b8af0e131ec26
jordan.osecki <jordan.osecki@c6e0ff0a-2120-11de-a108-cd2f117ce590>
2009-05-07 03:07:16 +0000

    Made Proc extend Element, added toString and eval

--- trunk/A4/Proc.h
+++ trunk/A4/Proc.h
@@ -1,26 +1,37 @@
+/**
+ * Program:  Proc.h
+ * Authors:  Group 7: Jordan Osecki, Geoff Oxholm, Alimoor Reza
+ * Class:    CS550, Assignment 4, Spring 2009
+**/
+
 #ifndef PROC_H
 #define PROC_H
 
+#include <iostream>
 #include <map>
 #include <string>
 #include "StmtList.h"
+#incldue "Element.h"
 
 using namespace std;
 
 class Expr;
 
+class Proc : public Element {
 
-class Proc
-{
 public:
-    Proc(list<string> *PL, StmtList *SL);
-    ~Proc() {delete SL_; };
-		// Changed environment table herE
-		Element* apply(map<string,Element*> &NT, map<string,Proc*> &FT, list<Expr*> *EL);
+    	Proc(list<string> *PL, StmtList *SL);
+    	~Proc() {delete SL_; };
+		
+	virtual string toString() const;
+  	virtual Element* eval(map<string,Element*> NT, map<string,Proc*> FT) const;
+
+	Element* apply(map<string,Element*> &NT, map<string,Proc*> &FT, list<Expr*> *EL);
+
 private:
-    StmtList *SL_;
-    list<string> *PL_;
-    int NumParam_;
+    	StmtList *SL_;
+    	list<string> *PL_;
+    	int NumParam_;
 };
 
 #endif
