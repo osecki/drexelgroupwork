534e6adcabae8cc6ec215a4d6a6aee049b7c972b
jordan.osecki <jordan.osecki@c6e0ff0a-2120-11de-a108-cd2f117ce590>
2009-05-27 04:21:24 +0000

    Linking just about finished...

--- trunk/A6/Program.cpp
+++ trunk/A6/Program.cpp
@@ -2,6 +2,7 @@
 #include <iostream>
 #include <iomanip>
 #include <fstream>
+#include <sstream>
 #include <vector>
 #include "Program.h"
 
@@ -20,38 +21,37 @@ Program::Program(StmtList *SL)
 
 void Program::dump()
 {
-	cout << endl << endl;
-	cout << setw(20) << right << "Symbol" << " | " << left << "Value" << right << "Type" << left << "Address" << endl;
-	cout << setfill('-') << setw(22) << right << "+" << setw(20) << "-" << setfill(' ') << endl;
-
-    //for (map<string,int>::iterator p = SymbolTable_.begin(); p != SymbolTable_.end(); p++) {
-    //    cout << setw(20) << right << p->first << " | " << left << p->second << endl;
-    //}
-
-	cout<<"Symbol Table	"<<endl;
-	cout<<"------------"<<endl;
+	cout << "Symbol Table" << endl;
+	cout << "Symbol		Value		Type		Address" << endl;
 	for (map<string, SymbolDetails>::iterator sti = symbolTable.begin(); sti!=symbolTable.end(); sti++) {
-		cout<<(*sti).first<<"	"<<(*sti).second.getValue() << (*sti).second.getType() << (*sti).second.getAddress() << endl;
+		cout<<(*sti).first<<"		"<<(*sti).second.getValue() << "		" << (*sti).second.getType() << "		" << (*sti).second.getAddress() << endl;
 	}
 
-	cout<<"constant table"<<endl;
-	cout<<"--------------"<<endl;
-	for (map<int, string>::iterator cti = constantValues.begin(); cti!=constantValues.end(); cti++) {
-		cout<<(*cti).first<<"	"<<(*cti).second<<endl;
-	}
-
-	cout << "PRINTING OUT VECTOR:" << endl;
+  cout << endl;
+	cout << "Program below and in prog.txt:" << endl;
+	cout << endl;
 
   for (int i=0; i< ralProgram.size(); i++)
 	     cout << ralProgram[i] << endl;
 
-	ofstream myfile;
-	myfile.open ("output.txt");
-
+	ofstream progFile;
+	progFile.open ("prog.txt");
 	for (int i=0; i< ralProgram.size(); i++)
-		myfile << ralProgram[i] << endl;
+		progFile << ralProgram[i] << endl;
+	progFile.close();
+
+	cout << endl;
+	cout << "Memory file created in mem.txt" << endl;
+
+	ofstream memFile;
+	memFile.open ("mem.txt");
 	
-	myfile.close();
+	for (map<string, SymbolDetails>::iterator j = symbolTable.begin(); j != symbolTable.end(); j++) {
+		memFile << (*j).second.getAddress() << "  " << (*j).second.getValue() << endl;
+	}	
+
+	memFile.close();
+
 }
 
 void Program::translate()
@@ -67,10 +67,8 @@ void Program::fixLabels()
 	// Make sure no labels are on a line of their own
 	for ( int i = 0; i < ralProgram.size(); i++ )
 	{
-		cout << "HERE" << endl;
 		if ( ralProgram[i].find(":") == ralProgram[i].length() - 1)
 		{
-			cout << "here"<< endl;
 			// Combine elements
 			ralProgram[i] = ralProgram[i] + ralProgram[i+1];
 
@@ -87,7 +85,6 @@ void Program::optimize()
 	// If this is found, Delete the load
 	for ( int i = 0; i < ralProgram.size() - 1; i++ )
 	{
-		//cout << ralProgram[i].find("ST") << "-" << ralProgram[i+1].find("LD") << "-" << (ralProgram[i].substr(3)).compare(ralProgram[i+1].substr(3)) << endl;
 		if ( ralProgram[i].find("ST") == 0 && ralProgram[i+1].find("LD") == 0 &&
 				 (ralProgram[i].substr(3)).compare(ralProgram[i+1].substr(3)) == 0 )
 		{
@@ -100,7 +97,79 @@ void Program::optimize()
 
 void Program::link()
 {
-	// TODO
+	// Loop through the symbol table three times, giving each addresses
+	int counter = 1;
+
+	// Assign Constants an address
+	for (map<string, SymbolDetails>::iterator a = symbolTable.begin(); a != symbolTable.end(); a++) {
+		    if ( ((*a).second.getType()).compare("Constant") == 0 )
+				{
+					(*a).second.setAddress(counter);
+					counter++;
+				}
+	}
+
+	// Assign Variables an address
+	for (map<string, SymbolDetails>::iterator b = symbolTable.begin(); b != symbolTable.end(); b++) {
+		  if ( ((*b).second.getType()).compare("Variable") == 0 )
+			{
+				(*b).second.setAddress(counter);
+			  counter++;
+			} 
+	}
+
+	// Assign Temporaries an address
+	for (map<string, SymbolDetails>::iterator c = symbolTable.begin(); c != symbolTable.end(); c++) {
+	  if ( ((*c).second.getType()).compare("Temporary") == 0 )
+		{
+			(*c).second.setAddress(counter);
+			counter++;
+		} 
+	}
+
+	// Calculate the addresses for labels
+	map<string, int> labelValues;
+	for ( int i = 0; i < ralProgram.size(); i++ )
+	{
+		if ( ralProgram[i].find(":") != string::npos )
+		{
+			// First enter into map
+			labelValues[ralProgram[i].substr(0,ralProgram[i].find(":"))] = i + 1;
+
+			// Next delete from that one
+			ralProgram[i] = ralProgram[i].substr(ralProgram[i].find(":") + 1);
+		}
+	}
+
+	// Time to actually do the linking in the program
+	for ( int j = 0; j < ralProgram.size(); j++ )
+	{
+		string tempVar = ralProgram[j].substr(ralProgram[j].find(" ") + 1);
+
+		if ( ralProgram[j].find("HLT") == string::npos)
+		{
+		// Need to decide if we look in symbolTable or labelTable
+		if ( (tempVar.substr(0, 1)).compare("L") == 0 )
+		{
+			int t1 = labelValues[tempVar];
+			string s1;
+			stringstream out1;
+			out1 << t1;
+			s1 = out1.str();
+			ralProgram[j] = ralProgram[j].substr(0, ralProgram[j].find(" ") + 1) + s1;
+		}
+		else
+		{
+			int t2 = symbolTable[tempVar].getAddress();
+			string s2;
+		  stringstream out2;
+		  out2 << t2;
+		  s2 = out2.str();
+			ralProgram[j] = ralProgram[j].substr(0, ralProgram[j].find(" ") + 1) + s2;
+		}
+	}
+	}
+
 }
 
 void Program::compile()
