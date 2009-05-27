584547d5b0e8704db41e5af5f5afbbe5ec1db46e
jordan.osecki <jordan.osecki@c6e0ff0a-2120-11de-a108-cd2f117ce590>
2009-05-27 02:16:17 +0000

    Implemented half of optimize

--- trunk/A6/Plus.cpp
+++ trunk/A6/Plus.cpp
@@ -17,12 +17,23 @@ Plus::Plus(Expr* op1, Expr* op2)
 string Plus::translate(map<int, string> &constantValues, map<string, SymbolDetails> &symbolTable, vector<string> &ralProgram) const
 {
 	// Handle two operands
-	string temp1 = op1_->translate(constantValues, symbolTable, ralProgram);
-	string temp2 = op2_->translate(constantValues, symbolTable, ralProgram);
-
-	// Handle Load and operator
-	ralProgram.push_back("LD " + temp1);
-	ralProgram.push_back("ADD " + temp2);
+  string temp1, temp2;
+	if ( OPTIMIZE)
+	{
+		string temp1 = op2_->translate(constantValues, symbolTable, ralProgram);
+		string temp2 = op1_->translate(constantValues, symbolTable, ralProgram);
+	
+		 ralProgram.push_back("LD " + temp2);
+		 ralProgram.push_back("ADD " + temp1);			 
+	}
+	else
+	{
+		string temp1 = op1_->translate(constantValues, symbolTable, ralProgram);
+		string temp2 = op2_->translate(constantValues, symbolTable, ralProgram);
+	
+		 ralProgram.push_back("LD " + temp1);
+		 ralProgram.push_back("ADD " + temp2);			 
+	}
 
 	// Handle Final Store and Temporary Variable Creation
 	Program P;
